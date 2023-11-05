d1ca834af3707704eb4530f04275552d18f2b9fb
ArturNunyan <118257851+ArturNunyan@users.noreply.github.com>
2023-11-05 18:45:27 +0400

    data css

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ function App() {
       amount: 999.99,
     },
     {
-      date: new Date(2022, 2, 12),
+      date: new Date(2023, 2, 15),
       description: 'MacBook',
       amount: 1245.72,
     },
     {
-      date: new Date(2024, 2, 12),
+      date: new Date(2023, 2, 17),
       description: 'Jeans',
       amount: 49.99,
     },
@@ -24,9 +24,9 @@ function App() {
   return (
    <div>
      <h1>let's bigin</h1>
-     <CostItem date={costs[0].date} description={costs[0].description} amount={costs[0].amount}></CostItem>
-     <CostItem date={costs[1].date} description={costs[1].description} amount={costs[1].amount}></CostItem>
-     <CostItem date={costs[2].date} description={costs[2].description} amount={costs[2].amount}></CostItem>
+     <CostItem date={costs[0].date} description={costs[0].description} amount={costs[0].amount}/>
+     <CostItem date={costs[1].date} description={costs[1].description} amount={costs[1].amount}/>
+     <CostItem date={costs[2].date} description={costs[2].description} amount={costs[2].amount}/>
    </div>
   );
 }
--- src/components/CostIntem.js
+++ src/components/CostIntem.js
@@ -1,14 +1,13 @@
+import CostDate from './CostDate';
 import './costitem.css'
 
 function CostItem(props) {
 
-    // const costData = new Date(2023, 2, 12);
-    // const costDescription = 'Ref';
-    // const costAmount = 999.99;
+   
 
     return (
         <div className='cost-item'>
-            <div>{props.data}</div>
+            <CostDate date={props.date}/>
             <div className='cost-item__description'>
                 <h2>{props.description}</h2>
                 <div className='cost-item__price'>${props.amount}</div>
