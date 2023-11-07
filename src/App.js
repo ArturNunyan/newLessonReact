d57377a76012de6224919337232199e9d8c86271
ArturNunayn <nunyanartur@gmail.com>
2023-11-07 17:49:27 +0400

    07.11

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import CostItem from "./components/CostIntem";
+import Costs from "./components/Costs";
 
 function App() {
 
@@ -24,9 +24,7 @@ function App() {
   return (
    <div>
      <h1>let's bigin</h1>
-     <CostItem date={costs[0].date} description={costs[0].description} amount={costs[0].amount}/>
-     <CostItem date={costs[1].date} description={costs[1].description} amount={costs[1].amount}/>
-     <CostItem date={costs[2].date} description={costs[2].description} amount={costs[2].amount}/>
+      <Costs costs={costs}/>
    </div>
   );
 }
